f29b21ac842517732c1a2d7a3df98d7bfb8b842f
Lucas MALANDRINO GUYOT LM271425 <lucas.malandrinoguyot@cea.fr>
2023-10-23 09:51:35 +0200

    Replace bufferline with barbar

---
+++ lua/configs/barbar.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrev<CR>')
+vim.keymap.set('n', '<leader>x', '<Cmd>BufferClose<CR>')
--- lua/keymap.lua
+++ lua/keymap.lua
@@ -5,7 +5,3 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
-vim.keymap.set('n', '<leader>x', '<cmd>bdelete<CR>', { desc = 'Close current buffer' })
-vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', { desc = 'Previous buffer' })
