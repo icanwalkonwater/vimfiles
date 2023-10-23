7c171fd43e23df2296d5104692fafa7183f6ce5d
Lucas MALANDRINO GUYOT LM271425 <lucas.malandrinoguyot@cea.fr>
2023-10-23 10:55:14 +0200

    Disable autopairs for rust lifetimes

--- lua/configs/mini.lua
+++ lua/configs/mini.lua
@@ -1,6 +1,10 @@
 require('mini.cursorword').setup({})
 require('mini.move').setup({})
-require('mini.pairs').setup({})
+require('mini.pairs').setup({
+  mappings = {
+    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^\\<].', register = { cr = false } }
+  }
+})
 require('mini.trailspace').setup({})
 
 local map = require('mini.map')
