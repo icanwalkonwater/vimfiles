ab8a1156752c8cdab4e50ffc9805c799c2dd2ba2
Lucas Malandrino <lucas.malandrino@gmail.com>
2024-01-23 11:00:57 +0100

    feat: Made zen mode wider

--- lua/configs/zen.lua
+++ lua/configs/zen.lua
@@ -1,2 +1,6 @@
-require('zen-mode').setup({})
+require('zen-mode').setup({
+  window = {
+    width = 180
+  }
+})
 vim.keymap.set('n', '<leader><leader>z', function() require('zen-mode').toggle() end, { desc = 'Toggle zen mode' })
