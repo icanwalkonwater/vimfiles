b4bd1692ba6dbf41b8152b81811e02fb245e186f
Lucas Malandrino <lucas.malandrino@gmail.com>
2025-02-27 12:33:35 +0100

    feat(@lsp): adds slang LSP

--- init.lua
+++ init.lua
@@ -705,6 +705,8 @@ require('lazy').setup({
 
         bashls = {},
 
+        slangd = {},
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -733,6 +735,7 @@ require('lazy').setup({
         'ts_ls',
         'nil_ls',
         'bashls',
+        'slangd',
       }
 
       -- Ensure the servers and tools above are installed
@@ -796,7 +799,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, shaderslang = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
