51be5739b25bf0e9809e9ced82dd8a864b46b254
Lucas Malandrino <lucas.malandrino@gmail.com>
2025-04-17 23:18:55 +0200

    feat(@lsp): adds jsonnet support

--- init.lua
+++ init.lua
@@ -666,6 +666,8 @@ require('lazy').setup({
             },
           },
         },
+
+        jsonnet_ls = {},
       }
 
       -- To avoid conflicts with mason's installation process and locally installed LSPs, we exclude some servers from being handled by mason.
@@ -681,6 +683,7 @@ require('lazy').setup({
         'nil_ls',
         'bashls',
         'slangd',
+        'jsonnet_ls',
       }
 
       -- Ensure the servers and tools above are installed
